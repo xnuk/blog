d7f034226f302e72c28ea9963e1c0b793bbdc13f
Xnuk Shuman <limm1127@gmail.com>
2016-09-20 09:20:24 +0900

    paragragh anchor: aria-hidden to aria-label
    + cite styling

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
-[].slice.call(document.querySelectorAll('h1,h2,h3,h4,h5,h6')).forEach(function(v){
+(function(){
+
+var messages={
+	// 엉엉 나도 템플릿 쓰고 십따
+	en: {
+		paragraph_anchor: function(v){'Link to paragraph: '+v}
+	},
+	ko: {
+		paragraph_anchor: function(v){'단락 링크: '+v}
+	}
+}
+var lang=document.documentElement.lang
+;[].slice.call(document.querySelectorAll('h1,h2,h3,h4,h5,h6')).forEach(function(v){
 	var a=document.createElement('a')
 	a.textContent='§'
 	a.setAttribute('href', '#'+v.id)
 	a.setAttribute('class', 'self')
-	a.setAttribute('aria-hidden', 'true')
+	a.setAttribute('aria-label', messages[lang].paragraph_anchor(v.textContent))
 	v.insertBefore(a, v.firstChild)
 })
 if(!(bowser.firefox || bowser.safari && !bowser.ios)){
@@ -11,3 +23,5 @@ if(!(bowser.firefox || bowser.safari && !bowser.ios)){
 	s.src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=MML_CHTML"
 	document.body.appendChild(s)
 }
+
+})()
