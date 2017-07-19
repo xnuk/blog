6fb951439566f9043053ff75c9c15a854011c8ab
Xnuk Shuman <limm1127@gmail.com>
2017-07-20 05:12:19 +0900

    add video extension

--- build.pl
+++ build.pl
@@ -1,4 +1,4 @@
-foreach (grep {$_ =~ /\.(?:png|jpg|gif)$/} map {chomp; $_} `find posts/`){
+foreach (grep {$_ =~ /\.(?:png|jpg|gif|mp4|webm)$/} map {chomp; $_} `find posts/`){
 	m|^posts/(.+/)?([^/]+)$|;
 	`mkdir -p dest/$1; cp posts/$1$2 dest/$1;`
 }
